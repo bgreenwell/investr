854de0faac2642f6092fcb139342bc27fe2579d9
Brandon Greenwell <greenwell.brandon@gmail.com>
2014-04-14 13:18:22 -0400

    Minor changes

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -1,1 +1,9 @@
+## TODO: Test that predictions and prediction standard errors are correct. The
+##       values produced by predict2.nls should be compared to the output from 
+##       SAS or JMP.
+
 context("Prediction")
+
+# test_that("predictions and prediction standard errors are correct", {
+#   
+# }
