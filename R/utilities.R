ddb321b102e18d7bf449a5f3385c7eb7cde74668
w108bmg <greenwell.brandon@gmail.com>
2015-01-06 11:48:05 -0500

    Back to development mode.

--- R/utilities.R
+++ R/utilities.R
@@ -5,7 +5,7 @@
 ##' 
 ##' @keywords internal
 AnyNA <- function(x) {
-#   if(getRversion() >= 3.1.0) {
+#   if(getRversion() >= "3.1.0") {
 #     return(anyNA(x))  # use built-in function if available
 #   } else {
 #     return(any(is.na(x)))
