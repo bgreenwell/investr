a27573d79c5725a9cf35c82e109ef046586f79c4
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-03-28 11:58:09 -0400

    minor fixes

--- tests/testthat/test-predFit.R
+++ tests/testthat/test-predFit.R
@@ -1,10 +1,10 @@
-## The following tests are for testing the prediction function and estimated
-## standard errors of the fitted values
+# The following tests are for testing the prediction function and estimated
+# standard errors of the fitted values
 context("Prediction")
 
 test_that("results from predFit match results from PROC NLIN in SAS", {
   
-  ## DNase data from the dataframes package
+  # DNase data from the dataframes package
   DNase1 <- data.frame(conc = c(0.04882812, 0.04882812, 0.19531250, 0.19531250, 
                                 0.39062500, 0.39062500, 0.78125000, 0.78125000, 
                                 1.56250000, 1.56250000, 3.12500000, 3.12500000, 
@@ -23,13 +23,13 @@ test_that("results from predFit match results from PROC NLIN in SAS", {
   DNase1.pred2 <- predFit(DNase1.nls, se.fit = TRUE, newdata = DNase1.new, 
                           interval = "prediction")
   
-  ## Fitted value standard errors from PROC NLIN in SAS/STATS
+  # Fitted value standard errors from PROC NLIN in SAS/STATS
   SAS.se.fit <- c(0.002899, 0.002899, 0.006079, 0.006079, 0.007461, 0.007461, 
                   0.007704, 0.007704, 0.007581, 0.007581, 0.009277, 0.009277,
                   0.009038, 0.009038, 0.012925, 0.012925)
   SAS.se.fit.new <- c(0.008857, 0.012272, 0.007548, 0.010014, 0.009314)
   
-  ## Confidence limits from PROC NLIN in SAS/STATS
+  # Confidence limits from PROC NLIN in SAS/STATS
   SAS.conf.lwr <- c(0.02442, 0.02442, 0.09892, 0.09892, 0.19246, 0.19246,
                     0.35768, 0.35768, 0.61640, 0.61640, 0.96082, 0.96082,
                     1.34799, 1.34799, 1.68706, 1.68706)
@@ -39,7 +39,7 @@ test_that("results from predFit match results from PROC NLIN in SAS", {
   SAS.conf.lwr.new <- c(1.48029, 1.67025, 0.43872, 1.58988, 1.27678)
   SAS.conf.upr.new <- c(1.51856, 1.72327, 0.47133, 1.63315, 1.31703)
   
-  ## Prediction limits from PROC NLIN in SAS/STATS
+  # Prediction limits from PROC NLIN in SAS/STATS
   SAS.pred.lwr <- c(-0.01126, -0.01126,  0.06855,  0.06855,  0.16409,  0.16409,  
                     0.32965,  0.32965,  0.58819,  0.58819,  0.93481,  0.93481,
                     1.32168,  1.32168,  1.66500,  1.66500)
@@ -49,7 +49,7 @@ test_that("results from predFit match results from PROC NLIN in SAS", {
   SAS.pred.lwr.new <- c(1.45376, 1.64754, 0.41047, 1.56474, 1.25081)
   SAS.pred.upr.new <- c(1.54510, 1.74598, 0.49958, 1.65829, 1.34300)
   
-  ## Expectations for original data
+  # Expectations for original data
   expect_true(all.equal(round(DNase1.conf[, "se.fit"], 6), SAS.se.fit))
   expect_true(all.equal(round(DNase1.pred[, "se.fit"], 6), SAS.se.fit))
   expect_true(all.equal(round(DNase1.conf[, "lwr"], 5), SAS.conf.lwr, tol = 1e-05))
@@ -57,7 +57,7 @@ test_that("results from predFit match results from PROC NLIN in SAS", {
   expect_true(all.equal(round(DNase1.pred[, "lwr"], 5), SAS.pred.lwr, tol = 1e-05))
   expect_true(all.equal(round(DNase1.pred[, "upr"], 5), SAS.pred.upr, tol = 1e-05))
   
-  ## Expectations for new data
+  # Expectations for new data
   expect_true(all.equal(round(DNase1.conf2[, "se.fit"], 6), SAS.se.fit.new))
   expect_true(all.equal(round(DNase1.pred2[, "se.fit"], 6), SAS.se.fit.new))
   expect_true(all.equal(round(DNase1.conf2[, "lwr"], 5), SAS.conf.lwr.new, tol = 1e-05))
@@ -85,12 +85,12 @@ test_that("", {
   pred.stats <- predict(lm1)
 
   # Predictions and confidence intervals
-  pred.investr.se.conf <- predFit(lm1, interval = "confidence")
-  pred.stats.se.conf <- predict(lm1, interval = "confidence")
+  pred.investr.conf <- predFit(lm1, interval = "confidence")
+  pred.stats.conf <- predict(lm1, interval = "confidence")
 
   # Predictions and prediction intervals
-  pred.investr.se.pred <- predFit(lm1, interval = "prediction")
-  pred.stats.se.pred <- predict(lm1, interval = "prediction")
+  pred.investr.pred <- predFit(lm1, interval = "prediction")
+  pred.stats.pred <- predict(lm1, interval = "prediction")
 
   # Predictions and standard errors
   pred.investr.se <- predFit(lm1, se.fit = TRUE)
