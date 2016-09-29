84bac1223b315dca79cc7562b38b0169a79de6eb
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-09-29 10:25:50 -0400

    added comments to code

--- R/utils.R
+++ R/utils.R
@@ -6,12 +6,14 @@ makeData <- function(x, label) {
 
 #' @keywords internal
 makeX <- function(object, newdata) {
+  # Fixed effects model matrix
   stats::model.matrix(eval(object$call$fixed)[-2], data = newdata)
 }
 
 
 #' @keywords internal
 makeZ <- function(object, newdata) {
+  # Random effects model matrix
   Q <- object$dims$Q  # number of grouping levels
   mCall <- object$call  # list containing image of the nlme call
   fixed <- eval(eval(mCall$fixed)[-2])  # fixed effects formula
@@ -26,12 +28,17 @@ makeZ <- function(object, newdata) {
 
 #' @keywords internal
 sigma.lme <- function(object, ...) {
+  # Estimated standard deviation of the within-group error
   object$sigma
 }
 
 
 #' @keywords internal
 varY <- function(object, newdata) {
+  
+  # FIXME: What if object$call$correlation is not NULL?
+  
+  # Unconditional response variance: Var[Y] = Var[X*beta + Z*alpha + error]
   Zmat <- makeZ(object, newdata)  # random effects design matrix
   Gmat <- nlme::getVarCov(object)  # random effects variance-covariance matrix
   var.y <- Zmat %*% Gmat %*% t(Zmat) + stats::sigma(object) ^ 2  # ZGZ' + (sigma^2)I
